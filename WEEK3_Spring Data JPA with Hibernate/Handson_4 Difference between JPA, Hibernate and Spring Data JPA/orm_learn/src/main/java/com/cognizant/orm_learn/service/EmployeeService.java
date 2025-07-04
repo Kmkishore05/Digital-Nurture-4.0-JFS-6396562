a4b10245dcdaedd37821e98742eeb657ed6bc395
KISHORE K M <119513011+Kmkishore05@users.noreply.github.com>
2025-07-04 22:55:23 +0530

    Add files via upload

---
+++ WEEK3_Spring Data JPA with Hibernate/Handson_4 Difference between JPA, Hibernate and Spring Data JPA/orm_learn/src/main/java/com/cognizant/orm_learn/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.cognizant.orm_learn.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.orm_learn.model.Employee;
+import com.cognizant.orm_learn.repository.EmployeeRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
