8e4644d3ef4c399a416f96032b421cb7454d2930
KISHORE K M <119513011+Kmkishore05@users.noreply.github.com>
2025-07-08 19:55:39 +0530

    Add files via upload

---
+++ WEEK4_Spring REST using Spring Boot/handson3_Hello World RESTful Web Service/spring-learn-2/src/main/java/com/cognizant/spring_learn_2/controller/Hellocontroller.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring_learn_2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellocontroller {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Hellocontroller.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        LOGGER.info("START - sayHello()");
+        String message = "Hello World!!";
+        LOGGER.info("END - sayHello()");
+        return message;
+    }
+}
